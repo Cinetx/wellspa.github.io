5631e7afc0167589b7ca084b50b1bd3bd582b030
Cinetx <39036064+Cinetx@users.noreply.github.com>
2020-01-09 06:09:07 +0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require("autoprefixer");
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+var stream = browserSync.stream;
+
+  gulp.task('style', function (done) {
+    gulp.src('./scss/style.scss')
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(postcss([
+        autoprefixer()
+      ]))
+      .pipe(gulp.dest('./css/'))
+  .pipe(browserSync.stream());
+    done();
+  });
+
+
+gulp.task('serve', function () {
+
+  browserSync.init({
+    server: {
+      baseDir: "./"
+
+    }
+
+  });
+
+  gulp.watch("*.html",  gulp.parallel('style')).on("change", reload);
+  gulp.watch("./scss/*.scss",  gulp.parallel('style')).on("change", reload);
+  // gulp.watch("./slick/*.css",  gulp.parallel('style')).on("change", reload);
+
+  // gulp.watch("./css/*.css").on("change", reload);
+
+});
